f88db7195211acdb49e4825623aea3800fc1a973
Mandar Mule <mandar.mule06@hotmail.com>
2016-06-07 22:32:34 +0530

    Checking in Assignments

---
+++ src/test/java/assignments/Assignment1.java
@@ -0,0 +1,11 @@
+package assignments;
+
+public class Assignment1 {
+
+	/*
+	 * 1. Navigate to http://live.guru99.com
+	 * 2. Click on Mobile and add iPhone to cart
+	 * 3. Apply Coupon on cart page.  Coupon Code: GURU50
+	 * 4. Verify 5% discount applied 
+	 * */
+}
