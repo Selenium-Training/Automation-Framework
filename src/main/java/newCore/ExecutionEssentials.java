bc3fa63d1439c67f18cf590173c5953b10ebb8a4
Mandar Mule <mandar.mule06@hotmail.com>
2016-07-31 14:34:37 +0530

    Log

--- src/main/java/newCore/ExecutionEssentials.java
+++ src/main/java/newCore/ExecutionEssentials.java
@@ -7,10 +7,12 @@ public class ExecutionEssentials {
 
 	public Browser browser;
 	public FrameworkProperties frameworkProperties;
-
-	public ExecutionEssentials(String browserType) throws IOException{
+	public ExecutionLog log;
+	
+	public ExecutionEssentials(final Object object, final String browserType) throws IOException{
 		browser = new Browser(browserType);
 		frameworkProperties = new FrameworkProperties();
+		log = new ExecutionLog(object);
 	}
 }
 
